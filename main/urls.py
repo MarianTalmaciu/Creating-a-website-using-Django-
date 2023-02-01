bc1b389ac5fd901af7948e04a8d2b0608d01f5d6
Marian <mariantalmaciu20@gmail.com>
2023-02-02 00:29:45 +0200

    covering templates and custom HTML

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("<str:name>", views.index, name="index"),
+path("<int:id>", views.index, name="index"),
+path("", views.home, name="home")
 # path("index2/", views.index2, name="index2"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,10 +4,12 @@ from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response, name):
-	ls = ToDoList.objects.get(name=name)
-	item = ls.item_set.get(id=1)
-	return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
+def index(response, id):
+	ls = ToDoList.objects.get(id=id)
+	return render(response, "main/list.html", {"ls":ls})
+
+def home(response):
+	return render(response, "main/home.html", {})
 
 # def index2(response):
 # 	return HttpResponse("<h1>I am python developer!</h1>")
